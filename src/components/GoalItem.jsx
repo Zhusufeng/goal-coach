4ac9a63770134132ac275d6fec848ea1c15c66ee
Lisa Gee <zhusufeng@gmail.com>
2018-03-11 15:07:57 -0700

    Add complete button & skeleton the helper func completeGoal

--- src/components/GoalItem.jsx
+++ src/components/GoalItem.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class GoalItem extends Component {
+
+  completeGoal () {
+    console.log()
+    // add to complete goals on the db
+    // remove this goal from the goals reference
+  }
+
   render () {
     console.log('this.props.goal', this.props.goal);
     const { email, title } = this.props.goal;
@@ -10,7 +17,13 @@ class GoalItem extends Component {
         style={{margin: '5px'}}
       >
         <strong>{title}</strong>
-        <span> submitted by <em>{email}</em></span>
+        <span style={{marginRight: '5px'}}> submitted by <em>{email}</em></span>
+        <button
+          className="btn btn-sm btn-primary"
+          onClick={() => this.completeGoal()}
+        >
+          Complete
+        </button>
       </div>
     );
   }
