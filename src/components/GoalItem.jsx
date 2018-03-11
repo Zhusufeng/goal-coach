ed20555b6107613b7ca2456cea7a8be71c50514a
Lisa Gee <zhusufeng@gmail.com>
2018-03-11 14:54:46 -0700

    Use this.props received from GoalList in GoalItem component

--- src/components/GoalItem.jsx
+++ src/components/GoalItem.jsx
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 
 class GoalItem extends Component {
   render () {
+    console.log('this.props.goal', this.props.goal);
+    const { email, title } = this.props.goal;
+
     return (
-      <div>Goal Item</div>
+      <div
+        style={{margin: '5px'}}
+      >
+        <strong>{title}</strong>
+        <span> submitted by <em>{email}</em></span>
+      </div>
     );
   }
 }
