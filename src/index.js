ebf43bef01332aa42d3619fc0d7a33b4a96a6d91
Lisa Gee <zhusufeng@gmail.com>
2018-03-10 21:01:43 -0800

    Add firebaseApp to App & listen to if signed in

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory} from 'react-router';
+import { firebaseApp } from './firebase';
 
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+
+// listener for firebase
+firebaseApp.auth().onAuthStateChanged(user => {
+  if (user) {
+    console.log('user has signed in or up', user);
+    // TODO let user to main app
+  } else {
+    console.log('user has signed out or still needs to sign in');
+    // TODO redirect user to sign up or sign in
+  }
+});
 
 ReactDOM.render(
   <Router path="/" history={browserHistory}>
