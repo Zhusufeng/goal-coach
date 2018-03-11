d4294d70faac3df545cd0013173e52cf60819e27
Lisa Gee <zhusufeng@gmail.com>
2018-03-10 20:38:49 -0800

    Import Router, Route & browserHistory from react-router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory} from 'react-router';
+
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
 ReactDOM.render(
-  <div>App</div>,
+  <Router path="/" history={browserHistory}>
+    
+  </Router>,
   document.querySelector('#root')
 );
