7e54a7e9864ea2f6aca8f3ef540839a2eadffe8e
Lisa Gee <zhusufeng@gmail.com>
2018-03-10 21:31:04 -0800

    Build skeleton of SignIn component

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
 
 class SignIn extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      error: {
+        message: ''
+      }
+    }
+  }
+
+
   render () {
     return (
-      <div>SignIn</div>
+      <div>
+        <h2>Sign In</h2>
+        <div>
+        <input
+          className=""
+          type="email"
+          placeholder="email" 
+          onChange={(e) => this.setState({email: e.target.value})}
+        />
+        <input
+          className=""
+          type="password"
+          placeholder="password" 
+          onChange={(e) => this.setState({password: e.target.value})}
+        />
+        <button>
+          Sign In
+        </button>
+        </div>
+        <div>
+          {this.state.error.message}
+        </div>
+      </div>
     );
   }
 }
