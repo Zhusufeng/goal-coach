5f2c150047f30e6d450b43732cfc3002c7589bb7
Lisa Gee <zhusufeng@gmail.com>
2018-03-11 01:22:33 -0800

    Add link to sign in in Sign Up component

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { firebaseApp } from '../firebase';
+import { Link } from 'react-router';
 
 class SignUp extends Component {
   constructor (props) {
@@ -51,6 +52,7 @@ class SignUp extends Component {
           </button>
         </div>
         <div>{this.state.error.message}</div>
+        <div><Link to={'/signin'}>Sign in instead</Link></div>
       </div>
     );
   }
